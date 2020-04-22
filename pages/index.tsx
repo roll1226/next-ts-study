315ae872750e066124a5005ad34c0722b2b4b9e7
roll1226 <tomo_yasu_4724@icloud.com>
2020-04-22 20:21:25 +0900

    [DELETE]pages

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,6 @@
-import React from "react"
-import Head from "next/head"
-import Component from "../components/index";
-
-type Props = {
-  title: string
-}
-
-class App extends React.Component<Props> {
-  static async getInitialProps(): Promise<Props> {
-    return { title: "Hello world" }
-  }
-
-  render() {
-    return (
-      <>
-        <Head>
-          <title>{this.props.title}</title>
-        </Head>
-        <Component />
-      </>
-    )
-  }
-}
-
-export default App
+import React from 'react'
+import { NextPage } from 'next'
+const Page: NextPage = props => (
+  <div>Welcome to next.js!</div>
+)
+export default Page
